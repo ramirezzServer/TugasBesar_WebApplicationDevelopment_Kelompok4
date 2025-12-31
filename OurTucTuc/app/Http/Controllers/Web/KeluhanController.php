51aec48c981a1bfd626b79c9acbece2789dbb63a
Fariss <faris651234@gmail.com>
2025-12-31 10:18:24 +0700

    ya

--- OurTucTuc/app/Http/Controllers/Web/KeluhanController.php
+++ OurTucTuc/app/Http/Controllers/Web/KeluhanController.php
@@ -10,7 +10,7 @@ class KeluhanController extends Controller
 {
     public function index()
     {
-        $keluhan = Keluhan::where('user_id', Auth::id())->latest()->get();
+        $keluhan = Keluhan::where('id_penumpang', Auth::id())->latest()->get();
         return view('user.keluhan.index', compact('keluhan'));
     }
 }
