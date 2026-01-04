86434665ff2f3b43fd884794a7857d7f3b9b498f
lintangmp <lin21lin08@gmail.com>
2026-01-04 23:01:22 +0700

    Bismillah

--- OurTucTuc/routes/web.php
+++ OurTucTuc/routes/web.php
@@ -204,11 +204,9 @@ Route::middleware(['auth', WebRoleMiddleware::class . ':admin'])
         Route::post('/jadwal-sopir', [AdminJS::class, 'store'])
             ->name('jadwal-sopir.store');
 
-<<<<<<< HEAD
+
         Route::get('/jadwal-sopir/{id}/update', [AdminJS::class, 'edit'])
-=======
-        Route::get('/jadwal-sopir/{id}/edit', [AdminJS::class, 'edit'])
->>>>>>> c707b74ac302242bfee1ce966d279ea4dbf0670d
+
             ->name('jadwal-sopir.edit');
 
         Route::put('/jadwal-sopir/{id}', [AdminJS::class, 'update'])
@@ -216,8 +214,6 @@ Route::middleware(['auth', WebRoleMiddleware::class . ':admin'])
 
         Route::delete('/jadwal-sopir/{id}', [AdminJS::class, 'destroy'])
             ->name('jadwal-sopir.destroy');
-<<<<<<< HEAD
+
     });
-=======
-    });
->>>>>>> c707b74ac302242bfee1ce966d279ea4dbf0670d
+
